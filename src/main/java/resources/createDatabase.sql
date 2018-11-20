4868d8cc6d9c7abec5da38f85ed325c4c1c2dd8a
Olexandr <olexandr.maltsev@gmail.com>
2018-11-20 15:36:30 +0200

    Adds correct web.xml to project and welcome jsp page

--- src/main/java/resources/createDatabase.sql
+++ src/main/java/resources/createDatabase.sql
@@ -1,1 +1,5 @@
-CREATE DATABASE `taskManager`;
+CREATE DATABASE `taskManager`;ina.startup.VersionLoggerListener.log Server built:          Nov 2 2018 14:27:55 UTC
+20-Nov-2018 15:22:22.969 INFO [main] org.apache.catalina.startup.VersionLoggerListener.log Server number:         9.0.13.0
+20-Nov-2018 15:22:22.969 INFO [main] org.apache.catalina.startup.VersionLoggerListener.log OS Name:               Linux
+20-Nov-2018 15:22:22.969 INFO [main] org.apache.catalina.startup.VersionLoggerListener.log OS Version:            4.15.0-39-generic
+20-Nov-2018 15:22:22.969 INFO [main] org.apache.catal
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Task List</title>
+</head>
+<body>
+<h2>Task List</h2>
+
+<ul>
+    <li><a href = "/tasks">Tasks</a></li>
+    <li><a href = "/about">About</a></li>
+</ul>
+</body>
+</html>
