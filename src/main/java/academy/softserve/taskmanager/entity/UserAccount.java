ff2303711915ebed8fe875b134aad2328946cac0
Olexandr <olexandr.maltsev@gmail.com>
2018-11-22 17:55:31 +0200

    Creates entity for user, adds sql script for user table creation

---
+++ src/main/java/academy/softserve/taskmanager/entity/UserAccount.java
@@ -0,0 +1,26 @@
+package academy.softserve.taskmanager.entity;
+
+public class UserAccount {
+
+    private String userName;
+    private String password;
+
+    public UserAccount() {
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ src/main/java/resources/createUserTable.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `taskManager`.`user` (
+  `email` VARCHAR(100) NOT NULL,
+  `password` VARCHAR(100) NOT NULL,
+  `user_name` VARCHAR(250) NULL,
+  PRIMARY KEY (`email`));
