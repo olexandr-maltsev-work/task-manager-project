7fab12d7e1de192bae7e830f5525a5c31bc94970
Olexandr Maltsev <olexandr.maltsev@gmail.com>
2018-11-24 17:02:10 +0200

    Adds task servlet filter

---
+++ src/main/java/academy/softserve/taskmanager/web/TaskServletFilter.java
@@ -0,0 +1,37 @@
+package academy.softserve.taskmanager.web;
+
+import academy.softserve.taskmanager.security.UserSession;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@WebFilter("/TaskServlet")
+public class TaskServletFilter implements Filter {
+
+    public void destroy() {
+    }
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
+        HttpServletRequest httpServletRequest = ((HttpServletRequest) request);
+        Object attribute = httpServletRequest.getSession().getAttribute("user");
+        if (attribute == null) {
+            request.getRequestDispatcher("WEB-INF/view/login.jsp").forward(request, response);
+            return;
+        }
+
+        String userId = (String) attribute;
+        if (!UserSession.isSessionValid(userId, httpServletRequest)) {
+            request.getRequestDispatcher("WEB-INF/view/404.jsp").forward(request, response);
+            return;
+        }
+
+        chain.doFilter(request, response);
+    }
+
+    public void init(FilterConfig config) throws ServletException {
+
+    }
+
+}
